537e24e2b650fe8a82dc0ea6a1cf77b5861e13c2
shrutii1010 <shruti.biradar818@gmail.com>
2024-01-01 22:54:59 +0530

    1st potd of 2024

---
+++ 1_455_Assign_Cookies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int m = g.size();
+        int n = s.size();
+        if (n == 0) {
+            return 0;
+        }
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+
+        while (i < m && j < n) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i;
+    }
+};
