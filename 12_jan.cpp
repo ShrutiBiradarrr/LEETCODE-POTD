23ea7096de47bd5f522b080badbf6c235f4a9440
shrutii1010 <shruti.biradar818@gmail.com>
2024-01-13 00:07:29 +0530

    updated

---
+++ 12_jan.cpp
@@ -0,0 +1,50 @@
+
+/*
+1704. Determine if String Halves Are Alike
+
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+Return true if a and b are alike. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+*/
+
+
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n=s.size();
+     
+        vector<int>v1,v2;
+        for(int i=0;i<n/2;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+                v1.push_back(s[i]);
+            }
+        }
+
+         for(int i=n/2;i<n;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+                v2.push_back(s[i]);
+
+            }
+        }
+       
+        if(v1.size()==v2.size()){
+            return true;
+        }
+
+        return false;
+    }
+};
